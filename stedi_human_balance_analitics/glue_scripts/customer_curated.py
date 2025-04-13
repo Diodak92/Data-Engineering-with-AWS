559cf66658ed661ac54fb913d3b4401918b4dde1
Diodak92 <tomasz.kowalski92@icloud.com>
2025-04-13 21:11:28 +0200

    Update of customer curated glue job

--- stedi_human_balance_analitics/glue_scripts/customer_curated.py
+++ stedi_human_balance_analitics/glue_scripts/customer_curated.py
@@ -33,14 +33,19 @@ Steptrainerlanding_node1744568552850 = glueContext.create_dynamic_frame.from_opt
 Customertrusted_node1744568555885 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "true"}, connection_type="s3", format="json", connection_options={"paths": ["s3://tomasz-project-stedi-lake-house/customer/trusted/"], "recurse": True}, transformation_ctx="Customertrusted_node1744568555885")
 
 # Script generated for node SQL Query
-SqlQuery8348 = '''
-SELECT c.*
-FROM customer_trusted c
-JOIN step_trainer_landing s
-ON c.serialNumber = s.serialNumber
-AND CAST(from_unixtime(s.sensorReadingTime / 1000) AS DATE) = c.lastUpdateDate;
+SqlQuery9623 = '''
+SELECT customerName, email, phone, birthday, serialnumber, registrationdate, lastupdatedate,
+sharewithresearchasofdate, sharewithpublicasofdate, sharewithfriendsasofdate
+FROM (
+    SELECT c.*, 
+    ROW_NUMBER() OVER(PARTITION BY c.email ORDER BY lastupdatedate) AS RN
+    FROM customer_trusted c
+    JOIN step_trainer_landing s
+    ON c.serialNumber = s.serialNumber
+    )
+WHERE RN = 1;
 '''
-SQLQuery_node1744568567092 = sparkSqlQuery(glueContext, query = SqlQuery8348, mapping = {"customer_trusted":Customertrusted_node1744568555885, "step_trainer_landing":Steptrainerlanding_node1744568552850}, transformation_ctx = "SQLQuery_node1744568567092")
+SQLQuery_node1744568567092 = sparkSqlQuery(glueContext, query = SqlQuery9623, mapping = {"customer_trusted":Customertrusted_node1744568555885, "step_trainer_landing":Steptrainerlanding_node1744568552850}, transformation_ctx = "SQLQuery_node1744568567092")
 
 # Script generated for node Customers curated
 EvaluateDataQuality().process_rows(frame=SQLQuery_node1744568567092, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1744566694575", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
