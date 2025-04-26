e7dc0e9249bc5a1c9d4a700fc38d9f294b0f86a3
Diodak92 <tomasz.kowalski92@icloud.com>
2025-04-26 22:11:12 +0200

    Adding glue DDL scripts

---
+++ stedi_human_balance_analitics/glue_DDL/accelerometer_landing.sql
@@ -0,0 +1,11 @@
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+	`user` string COMMENT 'from deserializer',
+	`timestamp` bigint COMMENT 'from deserializer',
+	`x` double COMMENT 'from deserializer',
+	`y` double COMMENT 'from deserializer',
+	`z` double COMMENT 'from deserializer'
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://tomasz-project-stedi-lake-house/accelerometer/landing/'
+TBLPROPERTIES ('classification' = 'json')
