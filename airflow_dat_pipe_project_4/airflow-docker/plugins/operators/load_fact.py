5204e5a2a032c35deaa0d80d55cc1c636e131a52
Diodak92 <tomasz.kowalski92@icloud.com>
2025-11-27 00:46:06 +0100

    Code for LoadFactOperator

--- airflow_dat_pipe_project_4/airflow-docker/plugins/operators/load_fact.py
+++ airflow_dat_pipe_project_4/airflow-docker/plugins/operators/load_fact.py
@@ -1,22 +1,31 @@
-from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
 
-class LoadFactOperator(BaseOperator):
+class LoadFactOperator(RedshiftDataOperator):
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                 cluster_identifier : str,
+                 database : str,
+                 db_user : str,
+                 sql : str, 
+                 aws_conn_id : str,
+                 poll_interval : str = 10,
+                 wait_for_completion=True,
+                 **kwargs,
+        ):
+        
+        super().__init__(
+            cluster_identifier=cluster_identifier,
+            database=database,
+            db_user=db_user,
+            sql=sql,
+            aws_conn_id=aws_conn_id,
+            poll_interval=poll_interval,
+            wait_for_completion=wait_for_completion,
+            **kwargs,
+        )
 
-        super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
-    def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
