82eaf0eb5b62a8f1378231741089a6838f74a766
Diodak92 <tomasz.kowalski92@icloud.com>
2025-12-06 23:09:16 +0100

    Added data quality check and dummy operator to run cheks in parallel

--- airflow_dat_pipe_project_4/airflow-docker/dags/final_project.py
+++ airflow_dat_pipe_project_4/airflow-docker/dags/final_project.py
@@ -115,13 +115,49 @@ def final_project():
 
     run_quality_checks = DataQualityOperator(
         task_id='Run_data_quality_checks',
+        workgroup_name=WORKGROUP_NAME,
+        database=DATABASE,
+        #sql_check = """SELECT * FROM public.staging_songs LIMIT 1;""",
+        sql_check= """
+                    WITH artists_stg AS (
+                    SELECT COUNT(DISTINCT(artist_id)) AS stg_artist_cnt
+                    FROM public.staging_songs
+                    ),
+                    artists AS (
+                    SELECT COUNT(DISTINCT(artistid)) AS artist_cnt
+                    FROM public.songs
+                    )
+                    SELECT artists_stg.stg_artist_cnt = artists.artist_cnt AS cnt_test
+                    FROM artists_stg, artists;
+                   """,
+        logic_test = "== True",
+        aws_conn_id='aws_credentials',
     )
+
+    run_song_quality_check = DataQualityOperator(
+        task_id='Run_song_null_check',
+        workgroup_name=WORKGROUP_NAME,
+        database=DATABASE,
+        #sql_check = """SELECT * FROM public.staging_songs LIMIT 1;""",
+        sql_check= """
+                    SELECT COUNT(title) AS null_cnt
+                    FROM public.songs
+                    WHERE (title IS NULL);
+                   """,
+        logic_test = "== 0",
+        aws_conn_id='aws_credentials',
+    )
+
+    quality_start = DummyOperator(task_id='Start_quality_checks')
+    quality_end = DummyOperator(task_id='End_quality_checks')
 
     start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
     [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
     load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, 
                              load_artist_dimension_table, load_time_dimension_table]
     [load_user_dimension_table, load_song_dimension_table, 
-     load_artist_dimension_table, load_time_dimension_table] # >> run_quality_checks
+     load_artist_dimension_table, load_time_dimension_table] >> quality_start
+    quality_start >> [run_quality_checks, run_song_quality_check] >> quality_end
+
 
 final_project_dag = final_project()
